2bc1ef1a0715b8fce7d609b0024fdd487f8d7ffb
LawrenceDarko <darkolawrence@gmail.com>
2021-11-01 10:49:44 +0800

    message page design and logic

---
+++ src/Pages/Messages.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Messages = () => {
+    return (
+        <View>
+            <Text>This is the Messages Page</Text>
+        </View>
+    )
+}
+
+export default Messages
