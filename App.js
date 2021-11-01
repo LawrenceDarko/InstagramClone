603f48fba52b574362714768ef312bd2950dcbf2
LawrenceDarko <darkolawrence@gmail.com>
2021-11-01 10:48:36 +0800

    App structure

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './src/BottomTabNavigation/Tabs'
+import { createStackNavigator } from '@react-navigation/stack';
+import ActivityPage from "./src/Pages/Activity";
+import MessagesPage from "./src/Pages/Messages";
+
+const PageStack = createStackNavigator();
 
 export default function App() {
+
+  const HomeTabs = () => {
+    return (
+      <Tabs />
+    )
+  }
+
+  const Activity = () => {
+    return (
+      <View>
+        <ActivityPage />
+      </View>
+    )
+  }
+
+  const Messages = () => {
+    return (
+      <View>
+        <MessagesPage />
+      </View>
+    )
+  }
+  
+
+  
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <PageStack.Navigator>
+        <PageStack.Screen name="HomeTabs" component={HomeTabs} options={{headerShown: false }}/>
+        <PageStack.Screen name="Activity" component={Activity} />
+        <PageStack.Screen name="Messages" component={Messages} />
+      </PageStack.Navigator>
+    </NavigationContainer>
   );
 }
 
