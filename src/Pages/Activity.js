13d964bc25f9a7278ca7594e6d799072363f6f82
LawrenceDarko <darkolawrence@gmail.com>
2021-11-01 10:49:11 +0800

    Activity page design and logic

---
+++ src/Pages/Activity.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Activity = () => {
+    return (
+        <View>
+            <Text>This is Activity Page</Text>
+        </View>
+    )
+}
+
+export default Activity
