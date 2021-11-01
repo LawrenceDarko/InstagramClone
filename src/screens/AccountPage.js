9acb014e31bfee7aa10f3276b78036f181e34dd8
LawrenceDarko <darkolawrence@gmail.com>
2021-11-01 10:50:40 +0800

    account page design and logic

---
+++ src/screens/AccountPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const AccountPage = () => {
+    return (
+        <View>
+            <Text>Accounts</Text>
+        </View>
+    )
+}
+
+export default AccountPage
