1e0112345d536e7e871d9598a5dc748f97fcb9d8
LawrenceDarko <darkolawrence@gmail.com>
2021-11-01 10:49:32 +0800

    Explore page design and logic

---
+++ src/Pages/Explore.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Explore = () => {
+    return (
+        <View>
+            <Text>Explore q</Text>
+        </View>
+    )
+}
+
+export default Explore
